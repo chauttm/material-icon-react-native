645961f5321b341e6d0796fbc92ca4dd08f56460
Chau Tran <chauttm@vnu.edu.vn>
2022-02-13 03:17:12 +0700

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Icon from "./MaterialIcons";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>r app!</Text>
       <StatusBar style="auto" />
+      <Icon name="add_a_photo" height="40" width="40" fill="#f0f" />
+      <Icon name="lock" fill="#0f0" height="24" width="24" />
+      <Icon name="lock" fill="#0f0" height="32" width="32" />
     </View>
   );
 }
@@ -14,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
