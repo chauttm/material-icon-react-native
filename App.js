b99926970dcbd11f6d9e5afb74e523756d93ea9a
Chau Tran <chauttm@vnu.edu.vn>
2022-02-14 15:47:34 +0700

    full icon set, all displayed in App

--- App.js
+++ App.js
@@ -6,11 +6,96 @@ import Icon from "./MaterialIcons";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>r app!</Text>
-      <StatusBar style="auto" />
+      account_circle
+      <Icon name="account_circle" height="40" width="40" fill="#f0f" />
+      account_circle_outlined
+      <Icon name="account_circle_outlined" height="40" width="40" fill="#f0f" />
+      add_a_photo
       <Icon name="add_a_photo" height="40" width="40" fill="#f0f" />
-      <Icon name="lock" fill="#0f0" height="24" width="24" />
-      <Icon name="lock" fill="#0f0" height="32" width="32" />
+      add_a_photo_outline
+      <Icon name="add_a_photo_outline" height="40" width="40" fill="#f0f" />
+      add_circle
+      <Icon name="add_circle" height="40" width="40" fill="#f0f" />
+      add_circle_outline
+      <Icon name="add_circle_outline" height="40" width="40" fill="#f0f" />
+      add_shopping_cart
+      <Icon name="add_shopping_cart" height="40" width="40" fill="#f0f" />
+      arrow_drop_down_round
+      <Icon name="arrow_drop_down_round" height="40" width="40" fill="#f0f" />
+      chevron_right
+      <Icon name="chevron_right" height="40" width="40" fill="#f0f" />
+      close
+      <Icon name="close" height="40" width="40" fill="#f0f" />
+      delete
+      <Icon name="delete" height="40" width="40" fill="#f0f" />
+      delete_round
+      <Icon name="delete_round" height="40" width="40" fill="#f0f" />
+      done
+      <Icon name="done" height="40" width="40" fill="#f0f" />
+      edit
+      <Icon name="edit" height="40" width="40" fill="#f0f" />
+      event
+      <Icon name="event" height="40" width="40" fill="#f0f" />
+      expand_more
+      <Icon name="expand_more" height="40" width="40" fill="#f0f" />
+      home
+      <Icon name="home" height="40" width="40" fill="#f0f" />
+      home_outline
+      <Icon name="home_outline" height="40" width="40" fill="#f0f" />
+      home_outline_round
+      <Icon name="home_outline_round" height="40" width="40" fill="#f0f" />
+      keyboard_backspace
+      <Icon name="keyboard_backspace" height="40" width="40" fill="#f0f" />
+      lock
+      <Icon name="lock" height="40" width="40" fill="#f0f" />
+      lock_outline
+      <Icon name="lock_outline" height="40" width="40" fill="#f0f" />
+      mail
+      <Icon name="mail" height="40" width="40" fill="#f0f" />
+      mail_outline
+      <Icon name="mail_outline" height="40" width="40" fill="#f0f" />
+      person
+      <Icon name="person" height="40" width="40" fill="#f0f" />
+      person_outline
+      <Icon name="person_outline" height="40" width="40" fill="#f0f" />
+      phone
+      <Icon name="phone" height="40" width="40" fill="#f0f" />
+      phone_round
+      <Icon name="phone_round" height="40" width="40" fill="#f0f" />
+      request_quote
+      <Icon name="request_quote" height="40" width="40" fill="#f0f" />
+      request_quote_outline
+      <Icon name="request_quote_outline" height="40" width="40" fill="#f0f" />
+      request_quote_outline_round
+      <Icon
+        name="request_quote_outline_round"
+        height="40"
+        width="40"
+        fill="#f0f"
+      />
+      rotate_left
+      <Icon name="rotate_left" height="40" width="40" fill="#f0f" />
+      shopping_cart
+      <Icon name="shopping_cart" height="40" width="40" fill="#f0f" />
+      shopping_cart_outline
+      <Icon name="shopping_cart_outline" height="40" width="40" fill="#f0f" />
+      shopping_cart_outline_round
+      <Icon
+        name="shopping_cart_outline_round"
+        height="40"
+        width="40"
+        fill="#f0f"
+      />
+      visibility
+      <Icon name="visibility" height="40" width="40" fill="#f0f" />
+      visibility_off
+      <Icon name="visibility_off" height="40" width="40" fill="#f0f" />
+      visibility_off_outline
+      <Icon name="visibility_off_outline" height="40" width="40" fill="#f0f" />
+      visibility_outline
+      <Icon name="visibility_outline" height="40" width="40" fill="#f0f" />
+      west_round
+      <Icon name="west_round" height="40" width="40" fill="#f0f" />
     </View>
   );
 }
